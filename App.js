62cecc032763cdcc6d6d2a21da389c334b48d118
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-01 15:55:19 +0600

    hands dirty with react-native-first-app !

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [outputText, setOutputText] = useState('React Native')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Submit" onPress={()=>setOutputText('Binary in my Blood !')} />
       <StatusBar style="auto" />
     </View>
   );
