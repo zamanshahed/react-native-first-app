4af8aed514e0ee39a5012d7c69405113b3b677d2
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-04-11 02:38:57 +0600

    added styling for the list items as well as scrollview added for the list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -18,19 +18,20 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.container}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Project Goals..."
+          style={styles.input}
           onChangeText={goalInputHandler}
           value={enterGoal}
           />
         <Button title="UPDATE"  onPress={addGoalHandler}/>
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal)=>
-          <Text key={goal}>{goal}</Text>
+          <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>
         )}
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -42,8 +43,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input:{
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem:{
+    padding: 5,
+    marginVertical: 5,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  },
   screen:{
-    padding:50,
-    flexDirection:'row'
+    padding:50
   },
 });
